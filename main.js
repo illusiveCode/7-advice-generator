49bfc8cbafd5eaee835fabd16fd60c67aa24ec71
Muhammad Raza <mh.raza2906@gmail.com>
2024-07-06 09:57:55 +0100

    feat: add advice slip fetch api

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const adviceNumber = document.getElementById("adviceNumber");
+const advice = document.getElementById("advice");
+const adviceGenButton = document.getElementById("dice");
 
+const getAdviceData = async () => {
+  const url = "https://api.adviceslip.com/advice";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+adviceGenButton.addEventListener("click", getAdviceData);
+
+//TODO Make a timeout
+//TODO function to change the advice number and advice itself
